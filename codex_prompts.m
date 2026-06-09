c852fcba766494a909c312fe8504e05ae992d29c
Amelia Bossom <abosso@uw.edu>
2026-06-08 20:15:29 -0700

    Add initial groundwater modeling prompts for Odessa

---
+++ codex_prompts.m
@@ -0,0 +1,30 @@
+First prompt asked:
+I am looking at the Odessa subarea groundwater and I want to model the conductivity in the basalt and the differebt type of rocks. Here are links about it http://columbia-institute.org/oa/odessa/Odessa_Aquifers.html https://dnr.wa.gov/washington-geological-survey/explore-popular-geology/geologic-provinces-washington/columbia-basin
+
+
+Here are the parameters for the odessa subarea: 
+West boundary head = 480 m
+East boundary head = 430 m
+Background nitrate = 2 mg/L nitrate-N
+Source-zone nitrate = 25 mg/L nitrate-N
+Background recharge = 2.0 x 10^-5 m/day
+Irrigated recharge = 2.0 x 10^-4 m/day
+Grande Ronde pumping = -3,000 m³/day per well
+Number of pumping cells = 10
+Total pumping = -30,000 m³/day
+
+
+
+x = east-west distance, 0 to 60 km
+y = north-south distance, 0 to 40 km
+z = elevation, 500 m to -600 m
+
+North: 47.51° N
+South: 47.15° N
+West: 119.09° W
+East: 118.29° W
+
+Odessa, WA center: 47.3332° N, 118.6908° W
+
+
+Use MODFLOW to create a model that shows how different types of rocks impact the flow of groundwater. Can you see if there are any ways to map the electrodynamics and thermodynamics in the rocks in this area. If so can you create a cross section of these properties using this corss section provided as a reference which is an odessa cross section
